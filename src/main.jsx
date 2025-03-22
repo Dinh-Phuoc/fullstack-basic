22be43a0589aad131863a3853f7583c5fe90bcef
Dinh Phuoc <100297381+Dinh-Phuoc@users.noreply.github.com>
2025-03-22 20:26:12 +0700

    Revert "Material UI"

--- src/main.jsx
+++ src/main.jsx
@@ -2,13 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { Experimental_CssVarsProvider as CssVarsProvider } from '@mui/material/styles'
-import theme from './assets/theme.js'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <CssVarsProvider theme={theme}>
-        <React.StrictMode>
-            <App />
-        </React.StrictMode>
-    </CssVarsProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 )
