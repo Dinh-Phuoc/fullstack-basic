1880bb353b60eed37f85d829317f923b7d199921
Dinh Phuoc <7pcsaru1st1.0@gmail.com>
2025-03-29 17:08:01 +0700

    Final

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,1 +1,12 @@
-export const API_ROOT = 'http://localhost:8017'
+let apiRoot = ''
+
+if (process.env.BUILD_MODE === 'dev') {
+    apiRoot = 'http://localhost:8017'
+}
+
+if (process.env.BUILD_MODE === 'prod') {
+    apiRoot = 'https://fullstack-basic-api.onrender.com'
+}
+
+export const API_ROOT = apiRoot
+
